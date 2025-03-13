c22e95b88107e933bff9928ff3c8beee38365e76
kirkiboi <168092735+kirkiboi@users.noreply.github.com>
2025-03-13 01:26:12 +0000

    with reset button and adjusted design

--- functions.js
+++ functions.js
@@ -3,11 +3,13 @@ const addButton = document.getElementById("add-up-btn");
 const minusBtn = document.getElementById("minus-btn");
 const firstDiv = document.getElementById("first-div");
 const secondDiv = document.getElementById("second-div");
+const resetBtn = document.getElementById("resetBTN");
 
 var counter = 0;
 
 addButton.addEventListener('click', add);
 minusBtn.addEventListener('click', minus);
+resetBtn.addEventListener('click', reset);
 
 function add(){
     counter++
@@ -24,3 +26,17 @@ function minus(){
         alert('try going the other way');
     }
 }
+function reset(){
+    counter = 0;
+    let currentNumber = document.getElementById("number").textContent = counter;
+}
+const nameButton = document.getElementById("name-button");
+nameButton.addEventListener('click', enterName);
+
+function enterName(){
+    var name = document.getElementById("name-holder").value;
+    let nameGreeting = document.getElementById("nameP").textContent = `Hi, ${name}. welcome!`;
+    let hideName = document.getElementById("name-holder").style.display = 'none';
+    nameButton.style.display = 'none';
+}
+console.log('I LOVE YOU, YOT!')
